706df6604ada5c210d59db84cc1763672025b021
j-sebas-29 <jsebas981029@gmail.com>
2026-09-03 17:24:16 -0500

    Commit de prueba

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Header } from './layout/header/header';
-import { Sidebar } from './layout/sidebar/sidebar';
 
 @Component({
 selector: 'app-root',
-imports: [RouterOutlet, Header, Sidebar],
+imports: [],
 templateUrl: './app.html',
 styleUrl: './app.scss'
 })
 export class App {
-protected readonly title = signal('imperia');
+	protected readonly menuOpen = signal(false);
+
+	protected toggleMenu(): void { this.menuOpen.update((open) => !open); }
+	protected closeMenu(): void { this.menuOpen.set(false); }
 }
